1055158bbd75ccfecb1af92b1bb6e9fdde3d030d
Mahfuzul Haque <68540132+MahfuzulHaque-22@users.noreply.github.com>
2022-10-18 11:42:45 +0600

    Add files via upload

---
+++ lib/screens/components/body.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'clock.dart';
+import 'country_card.dart';
+import 'time_in_hour_and_minute.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Text(
+              "Newport Beach, USA | PST",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            TimeInHourAndMinute(),
+            const Spacer(),
+            Clock(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
